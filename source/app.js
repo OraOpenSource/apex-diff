8d53f9aa56f3785bc323c3faccc41f3c2ecd9bd8
Martin D'Souza <martin.dsouza@gmail.com>
2015-09-27 12:21:44 -0600

    Major update/cleanup.

--- source/app.js
+++ source/app.js
@@ -1,14 +1,8 @@
 /**
- * TODO other comments
- * Config
- *  Config file is located in the config folder
- *  Look at the config_sample.json and create config.json with your connections
- *
+ * apex-diff
+ * oraopensource.com
+ * Project & license info: https://github.com/OraOpenSource/apex-diff
  */
-
-// TODO mdsouza: debug mode, output each step
-// TODO mdsouza: Put in parameter to handle sqlcl name (default to sql)
-
 
 /**
  * Wrapper for console statement
@@ -20,6 +14,13 @@ function debug(){
   }
 }//debug
 
+/**
+ * Logs time from start of app.js
+ */
+function logTime(){
+  debug('Time: ' + (new Date() - timer.start)/1000 + 's');
+}
+
 var
   //Requires
   fs = require('fs'),
@@ -28,8 +29,6 @@ var
   //Console
   log = console.log,
   childProcess,
-  //
-  spoolFilename = __dirname + '/apex_diff_generate_json.sql',
   //arguments for this process (first 2 are node and app.js)
   args = process.argv.slice(2), // Array of arguments
   arguments = {
@@ -55,21 +54,17 @@ var
       }
     }
   },
-
-
-
   timer = {
-    start : new Date(),
-    end : ''},
-  // TODO mdsouza: document these as options
+    start : new Date()
+  },
   options = {
     configFile : __dirname + '/../config/config.json',
     sqlcl : "sql", //Path to sqlcl
     debug : false,
-    // TODO mdsouza: better name?
-    rebuildFile : false // Rebuild the APEX json file
+    rebuildTempFile : false // Rebuild the APEX json file
   }
 ;
+
 // Look for config file
 if (!fs.existsSync(options.configFile)) {
   console.log('Missing config file: ' + options.configFile);
@@ -79,8 +74,7 @@ if (!fs.existsSync(options.configFile)) {
 else{
   // Load config file
   var configFileData = fs.readFileSync(options.configFile, 'utf8');
-  configFileData = JSON.parse(configFileData);
-  options = extend(options, configFileData);
+  options = extend(options, JSON.parse(configFileData));
   debug('options', options);
 }
 
@@ -106,11 +100,13 @@ else if (!options.connections[arguments.connectionName]) {
 }
 
 debug('Validations passed');
+logTime();
+
 
 debug('Setting up sql.command');
 
 // Determine if file should be rebuilt
-if (options.rebuildFile || !fs.existsSync(sql.files.path + '/' + sql.files.generateJson.fileName)){
+if (options.rebuildTempFile || !fs.existsSync(sql.files.path + '/' + sql.files.generateJson.fileName)){
   // Need to run the main apex-diff script to regenerate the JSON file
   debug('Rebuilding generateJson file');
   sql.command = sql.command.replace('%FILENAME%', sql.files.apexDiff.fileName);
@@ -127,12 +123,13 @@ sql.command = sql.command.replace('%SQLCL%', options.sqlcl);
 sql.command = sql.command.replace('%CONNECTION%', options.connections[arguments.connectionName]);
 sql.command = sql.command.replace('%DIRECTORY%', __dirname);
 sql.command = sql.command.replace('%APP_ID%', arguments.appId);
-sql.command = sql.command.replace('%SPOOL_FILENAME%', sql.files.generateJson.fileName);
+sql.command = sql.command.replace('%SPOOL_FILENAME%', sql.files.path + '/' + sql.files.generateJson.fileName);
 debug('sql.command: ' + sql.command);
 
 debug('Running sql.command');
 childProcess = execSync(sql.command,{ encoding: 'utf8' });
 debug(childProcess);
+logTime();
 
 debug('Reading .json file');
 sql.files.json.data = fs.readFileSync(sql.files.json.fileName, 'utf8');
@@ -140,62 +137,10 @@ sql.files.json.data = fs.readFileSync(sql.files.json.fileName, 'utf8');
 sql.files.json.data = JSON.parse(sql.files.json.data);
 //Prettify
 sql.files.json.data = (JSON.stringify(sql.files.json.data, null, 2));
+logTime();
 
 debug('Writing pretty JSON to file');
 fs.writeFileSync(sql.files.json.fileName, sql.files.json.data);
 
 // END
-debug('Time: ' + (new Date() - timer.start)/1000 + 's');
-
-// TODO mdsouza:
-process.exit(1);
-
-
-
-// TODO mdsouza: build run file only if not exists or option
-function prettyJSON(fileName){
-  console.log('prettyJSON');
-  fs.readFile(fileName, 'utf8', function (err,data) {
-    if (err) {
-      return console.log(err);
-    }
-
-    var
-      jsonData = JSON.parse(data), //Convert string to JSON
-      jsonText = (JSON.stringify(jsonData, null, 2)) //pretty JSON
-    ;
-
-    console.log('Writing update JSON file');
-    fs.writeFile(fileName, jsonText, function(err) {
-      if(err) {
-        return console.log(err);
-      }
-    });
-  });
-}// prettyJSON
-
-
-
-
-console.log('Calling sql command');
-childProcess = exec(sql.command,
-  function (error, stdout, stderr) {
-    if (error !== null) {
-      console.log('exec error: ' + error);
-    }
-    else{
-      // TODO mdsouza: delete run_apex_json.sql
-      prettyJSON('f' + apexAppId + '.json');
-
-      // var filePath = "c:/book/discovery.docx" ;
-      // TODO mdsouza: renable
-      // console.log('deleting temp filename');
-      // fs.unlinkSync(spoolFilename);
-      // console.log(__dirname);
-      // console.log(process.cwd());
-    }
-
-    // Wrap up
-    timer.end = new Date();
-    console.log('Total Time: ' + (timer.end - timer.start)/1000 + 's');
-});
+logTime();
