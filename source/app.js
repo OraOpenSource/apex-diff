00628e400bf77348526c9579fba66ed90fa57280
Martin D'Souza <martin.dsouza@gmail.com>
2015-09-27 08:32:54 -0600

    code backup. (still alpha)

--- source/app.js
+++ source/app.js
@@ -1,7 +1,156 @@
-// TODO mdsouza: comments
+/**
+ * TODO other comments
+ * Config
+ *  Config file is located in the config folder
+ *  Look at the config_sample.json and create config.json with your connections
+ *
+ */
 
 // TODO mdsouza: debug mode, output each step
 // TODO mdsouza: Put in parameter to handle sqlcl name (default to sql)
+
+
+/**
+ * Wrapper for console statement
+ *
+ */
+function debug(){
+  if (options.debug) {
+    log.apply(console.log, arguments );
+  }
+}//debug
+
+var
+  //Requires
+  fs = require('fs'),
+  execSync = require('child_process').execSync,
+  extend = require('util')._extend,
+  //Console
+  log = console.log,
+  childProcess,
+  //
+  spoolFilename = __dirname + '/apex_diff_generate_json.sql',
+  //arguments for this process (first 2 are node and app.js)
+  args = process.argv.slice(2), // Array of arguments
+  arguments = {
+    connectionName : args[0],
+    appId : args[1]
+  },
+  //SQL
+  sql = {
+    command : '%SQLCL% %CONNECTION% @%DIRECTORY%/%FILENAME% %PARAMS%',
+    files : {
+      path : __dirname,
+      json : {
+        fileName : 'f' + arguments.appId + '.json',
+        data : ''
+      },
+      apexDiff : {
+        fileName : 'apex-diff.sql',
+        params : '%APP_ID% %SPOOL_FILENAME%'
+      },
+      generateJson : {
+        fileName : 'apex_diff_generate_json.sql',
+        params : '%APP_ID%'
+      }
+    }
+  },
+
+
+
+  timer = {
+    start : new Date(),
+    end : ''},
+  // TODO mdsouza: document these as options
+  options = {
+    configFile : __dirname + '/../config/config.json',
+    sqlcl : "sql", //Path to sqlcl
+    debug : false,
+    // TODO mdsouza: better name?
+    rebuildFile : false // Rebuild the APEX json file
+  }
+;
+// Look for config file
+if (!fs.existsSync(options.configFile)) {
+  console.log('Missing config file: ' + options.configFile);
+  console.log('See TODO (doc link) for more info');
+  process.exit(1);
+}
+else{
+  // Load config file
+  var configFileData = fs.readFileSync(options.configFile, 'utf8');
+  configFileData = JSON.parse(configFileData);
+  options = extend(options, configFileData);
+  debug('options', options);
+}
+
+//General debug (can do here since value is set)
+debug('sql: ', sql);
+debug('arguments: ',  arguments);
+
+
+// VALIDATIONS
+// Check that argument has two parameters (connection and appid)
+if (!arguments.connectionName){
+  console.log('Missing connection string');
+  process.exit(1);
+}
+else if (!arguments.appId) {
+  console.log('Missing appId');
+  process.exit(1);
+}
+else if (!options.connections[arguments.connectionName]) {
+  console.log('Invalid connection: ' + arguments.connectionName);
+  console.log('Valid connections: ' + Object.keys(options.connections).toString());
+  process.exit(1);
+}
+
+debug('Validations passed');
+
+debug('Setting up sql.command');
+
+// Determine if file should be rebuilt
+if (options.rebuildFile || !fs.existsSync(sql.files.path + '/' + sql.files.generateJson.fileName)){
+  // Need to run the main apex-diff script to regenerate the JSON file
+  debug('Rebuilding generateJson file');
+  sql.command = sql.command.replace('%FILENAME%', sql.files.apexDiff.fileName);
+  sql.command = sql.command.replace('%PARAMS%', sql.files.apexDiff.params);
+}
+else{
+  debug('Just running generateJson');
+  sql.command = sql.command.replace('%FILENAME%', sql.files.generateJson.fileName);
+  sql.command = sql.command.replace('%PARAMS%', sql.files.generateJson.params);
+}
+
+//Finish sql.command replacement
+sql.command = sql.command.replace('%SQLCL%', options.sqlcl);
+sql.command = sql.command.replace('%CONNECTION%', options.connections[arguments.connectionName]);
+sql.command = sql.command.replace('%DIRECTORY%', __dirname);
+sql.command = sql.command.replace('%APP_ID%', arguments.appId);
+sql.command = sql.command.replace('%SPOOL_FILENAME%', sql.files.generateJson.fileName);
+debug('sql.command: ' + sql.command);
+
+debug('Running sql.command');
+childProcess = execSync(sql.command,{ encoding: 'utf8' });
+debug(childProcess);
+
+debug('Reading .json file');
+sql.files.json.data = fs.readFileSync(sql.files.json.fileName, 'utf8');
+//Convert string to JSON
+sql.files.json.data = JSON.parse(sql.files.json.data);
+//Prettify
+sql.files.json.data = (JSON.stringify(sql.files.json.data, null, 2));
+
+debug('Writing pretty JSON to file');
+fs.writeFileSync(sql.files.json.fileName, sql.files.json.data);
+
+// END
+debug('Time: ' + (new Date() - timer.start)/1000 + 's');
+
+// TODO mdsouza:
+process.exit(1);
+
+
 
 // TODO mdsouza: build run file only if not exists or option
 function prettyJSON(fileName){
@@ -25,39 +174,11 @@ function prettyJSON(fileName){
   });
 }// prettyJSON
 
-var
-  fs = require('fs'),
-  exec = require('child_process').exec,
-  child,
-  sqlConnnection = 'giffy/giffy@localhost:11521/xe', // TODO mdsouza: parameter
-  // sqlConnnection = 'insum/insum@localhost:11521/xe', // TODO mdsouza: parameter
-  // sqlConnnection = 'ohtech/OHt3ch98Y@einstein:1522/dev125', // TODO mdsouza: parameter
-  apexAppId = '113', // TODO mdsouza: parameter
-  // apexAppId = '815' // TODO mdsouza: parameter
-  sqlCommand = 'sqlcl %SQL_CONNECTION% @%DIRECTORY%/apex-diff %APP_ID% %SPOOL_FILENAME%',
-  // sqlCommand = 'sqlcl %SQL_CONNECTION% @%DIRECTORY%/apex_diff_generate_json.sql',
-  spoolFilename = __dirname + '/apex_diff_generate_json.sql',
-  timer = {
-    start : new Date(),
-    end : ''}
-;
-
-sqlCommand = sqlCommand.replace('%SQL_CONNECTION%', sqlConnnection);
-sqlCommand = sqlCommand.replace('%APP_ID%', apexAppId);
-sqlCommand = sqlCommand.replace('%DIRECTORY%', __dirname);
-sqlCommand = sqlCommand.replace('%SPOOL_FILENAME%', spoolFilename);
-
-//Test to see if file exists
-// TODO mdsouza:
-if (fs.existsSync(spoolFilename)) {
-    console.log('File Exists');
-}
 
 
-console.log('sqlCommand:', sqlCommand);
 
 console.log('Calling sql command');
-child = exec(sqlCommand,
+childProcess = exec(sql.command,
   function (error, stdout, stderr) {
     if (error !== null) {
       console.log('exec error: ' + error);
